055da93e6dd5ca1140480bbcc4e922a23267d9fb
zhouwei <zhouwei@hywintech.com>
2024-06-14 10:27:20 +0800

    feat: 包位置切换到java/下

--- src/main/java/cn/minalz/nio/c3/WalkJarFile.java
+++ src/main/java/cn/minalz/nio/c3/WalkJarFile.java
@@ -1,4 +1,4 @@
-package cn.minalz.nio.c4;
+package cn.minalz.nio.c3;
 
 import lombok.extern.slf4j.Slf4j;
 
--- src/main/java/cn/minalz/nio/c4/BlockServerDemo.java
+++ src/main/java/cn/minalz/nio/c4/BlockServerDemo.java
@@ -1,4 +1,4 @@
-package cn.minalz.nio;
+package cn.minalz.nio.c4;
 
 import lombok.extern.slf4j.Slf4j;
 
