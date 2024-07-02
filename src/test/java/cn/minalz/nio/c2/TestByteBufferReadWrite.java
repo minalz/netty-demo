5c9baea06c46faf7554cb9e46daddbd4a58f6f54
zhouwei <zhouwei@hywintech.com>
2024-07-02 10:17:14 +0800

    feat: add 简单的聊天室demo和rpc实现

---
+++ src/test/java/cn/minalz/nio/c2/TestByteBufferReadWrite.java
@@ -0,0 +1,21 @@
+package cn.minalz.nio.c2;
+
+import java.nio.ByteBuffer;
+
+public class TestByteBufferReadWrite {
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        buffer.put((byte) 0x61); // 'a'
+        ByteBufferUtil.debugAll(buffer);
+        buffer.put(new byte[]{0x62, 0x63, 0x64}); // b  c  d
+        ByteBufferUtil.debugAll(buffer);
+//        System.out.println(buffer.get());
+        buffer.flip();
+        System.out.println(buffer.get());
+        ByteBufferUtil.debugAll(buffer);
+        buffer.compact();
+        ByteBufferUtil.debugAll(buffer);
+        buffer.put(new byte[]{0x65, 0x6f});
+        ByteBufferUtil.debugAll(buffer);
+    }
+}
